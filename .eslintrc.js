13aaf7412e4097fc637d24efe3acc0daf1d75ece
Bruno Avelino <bruno.souza.avelino2@gmail.com>
2021-01-08 01:02:30 -0300

    editor

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,11 +28,7 @@ module.exports = {
     'plugin:prettier/recommended' // Make sure this is always the last element in the array.
   ],
   rules: {
-    'prettier/prettier': [
-      'error',
-      {},
-      { usePrettierrc: true, endOfLine: 'auto' }
-    ],
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
