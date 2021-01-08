ca6bcc7f12d421cfa3ed46d748ebdc00e8d574e9
Bruno Avelino <bruno.souza.avelino2@gmail.com>
2021-01-08 12:28:24 -0300

    PWA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,22 @@
+import Head from 'next/head'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Next.js - Boilerplate</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta
+          name="description"
+          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+        />
+        <html lang="pt-BR" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
