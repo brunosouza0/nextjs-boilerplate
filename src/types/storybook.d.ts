ade9f709e5d81a2cfe1b362bfb54701becac980b
Bruno Avelino <bruno.souza.avelino2@gmail.com>
2021-02-09 16:12:35 -0300

    storybook argstype type

--- src/types/storybook.d.ts
+++ src/types/storybook.d.ts
@@ -1,8 +1,42 @@
 import '@storybook/addons'
 
+type Types =
+  | 'array'
+  | 'boolean'
+  | 'number'
+  | 'range'
+  | 'object'
+  | 'radio'
+  | 'inline-radio'
+  | 'check'
+  | 'inline-check'
+  | 'select'
+  | 'multi-select'
+  | 'text'
+  | 'color'
+  | 'date'
+
 declare module '@storybook/addons' {
   export interface Parameters {
-    viewport?: { defaultViewport: 'mobile1' | 'mobile2' | 'tablet' }
-    backgrounds?: { default: 'light' | 'dark' }
+    viewport?: {
+      defaultViewport: 'mobile1' | 'mobile2' | 'tablet'
+      [key: sting]: any
+    }
+    backgrounds?: {
+      default: 'light' | 'dark'
+      [key: sting]: any
+    }
+  }
+
+  export interface ArgType {
+    control?: {
+      type: Types
+      options?: Array<any>
+      min?: number
+      max?: number
+      step?: number
+      separator?: string
+    }
+    action?: string
   }
 }
