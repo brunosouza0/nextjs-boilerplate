47e5f9fd3ce55b3eb1595c31f70301a2c2ac0e9c
Bruno Avelino <bruno.souza.avelino2@gmail.com>
2021-05-13 16:00:46 -0300

    radio

---
+++ src/components/Radio/stories.tsx
@@ -0,0 +1,26 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+
+import Radio, { RadioProps } from '.'
+
+export default {
+  title: 'Radio',
+  component: Radio,
+  args: {
+    name: 'radio'
+  },
+  parameters: {
+    backgrounds: { default: 'dark' }
+  },
+  argTypes: {
+    onCheck: { action: 'onCheck' }
+  }
+} as Meta<RadioProps>
+
+const Template: Story<RadioProps> = (args) => (
+  <div style={{ display: 'inline-flex', flexDirection: 'column', gap: '1rem' }}>
+    <Radio {...args} labelFor="radio1" value="radio1" label="Label 1" />
+    <Radio {...args} labelFor="radio2" value="radio2" label="Label 2" />
+  </div>
+)
+
+export const Default = Template.bind({})
