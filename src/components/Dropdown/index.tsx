29659926064c5d9f9fa066fe6b1935dd55539878
Bruno Avelino <bruno.souza.avelino2@gmail.com>
2021-05-25 13:40:15 -0300

    dropdown

---
+++ src/components/Dropdown/index.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import * as S from './styles'
+
+export type DropdownProps = {
+  title: React.ReactNode
+  children: React.ReactNode
+}
+
+const Dropdown = ({ title, children }: DropdownProps) => {
+  const [isOpen, setIsOpen] = useState(false)
+  return (
+    <S.Wrapper>
+      <S.Title onClick={() => setIsOpen(!isOpen)}>{title}</S.Title>
+      <S.Content isOpen={isOpen} aria-hidden={!isOpen}>
+        {children}
+      </S.Content>
+    </S.Wrapper>
+  )
+}
+
+export default Dropdown
